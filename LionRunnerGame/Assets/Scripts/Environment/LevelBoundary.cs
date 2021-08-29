9f4adb950a97c3cddd8cd2220e3e6a6e007860f2
TriadSyndicate <38569368+TriadSyndicate@users.noreply.github.com>
2021-08-30 02:50:24 +0300

    simba game

---
+++ LionRunnerGame/Assets/Scripts/Environment/LevelBoundary.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelBoundary : MonoBehaviour
+{
+    public static float leftSide = -1.9f;
+    public static float rightSide = 1.9f;
+    public float internalLeft;
+    public float internalRight;
+    // Update is called once per frame
+    void Update()
+    {
+        internalLeft = leftSide;
+        internalRight = rightSide;
+    }
+}
---
+++ LionRunnerGame/Assets/Scripts/PlayerMove.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMove : MonoBehaviour
+{
+    public float moveSpeed = 3;
+    public float horizontalSpeed = 6;
+    void Update()
+    {
+        transform.Translate(Vector3.forward * Time.deltaTime * moveSpeed, Space.World);
+        if (Input.GetKey(KeyCode.LeftArrow) || Input.GetKey(KeyCode.A))
+        {
+            if(this.gameObject.transform.position.x > LevelBoundary.leftSide)
+            {
+                transform.Translate(Vector3.left * Time.deltaTime * horizontalSpeed, Space.World);
+            }         
+        }
+        if (Input.GetKey(KeyCode.RightArrow) || Input.GetKey(KeyCode.D)) 
+        {   
+            if(this.gameObject.transform.position.x < LevelBoundary.rightSide)
+            {
+                transform.Translate(Vector3.right * Time.deltaTime * horizontalSpeed, Space.World);
+            }
+            
+        }
+    }
+}
