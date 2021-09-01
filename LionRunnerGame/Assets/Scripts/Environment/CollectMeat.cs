5110d3d106e58db225cdb76fd149d56dad4d71e1
TriadSyndicate <38569368+TriadSyndicate@users.noreply.github.com>
2021-09-01 06:18:58 +0300

    yet

---
+++ LionRunnerGame/Assets/Scripts/Environment/CollectMeat.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+using UnityEngine.UI;
+public class CollectMeat : MonoBehaviour
+{
+    public AudioSource munch;
+    public Text txtScore;
+   void Start()
+   {
+        Text txtScore = GameObject.Find("Canvas/Score").GetComponent<Text>();
+   }
+    void OnTriggerEnter(Collider other)
+    {
+            int init = int.Parse(txtScore.text);
+            munch.Play();
+            this.gameObject.SetActive(false);
+            init = init + 5;
+            txtScore.text = "" + init;
+    }
+    private readonly float FullRotation = 360f;
+        [SerializeField]
+        private float speed = 1f;
+        private float currentRotation = 0f;
+
+        public event EventHandler FullyRotated;
+        private bool IsFullRotation
+        {
+            get { return Mathf.Abs(currentRotation) >= FullRotation; }
+        } 
+
+    // Update is called once per frame
+    void Update()
+    {
+        Rotate();
+        RaiseEventOnFullRotation();
+        ResetRotationOnFullRotation();
+    }
+    private void Rotate()
+    {
+        transform.Rotate(0, speed, 0);
+        currentRotation += speed;
+    }
+    private void RaiseEventOnFullRotation()
+    {
+    if (IsFullRotation)
+    {
+        if (FullyRotated!=null)
+        {
+            FullyRotated(this, new EventArgs());
+        }
+    }
+    }
+    private void ResetRotationOnFullRotation()
+    {
+        if (IsFullRotation)
+        {
+        currentRotation -= FullRotation * Mathf.Sign(currentRotation);
+        }
+    }
+}
--- LionRunnerGame/Assets/Scripts/PlayerMove.cs
+++ LionRunnerGame/Assets/Scripts/PlayerMove.cs
@@ -1,29 +1,55 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class PlayerMove : MonoBehaviour
 {
-    public float moveSpeed = 3;
+    public float moveSpeed = 10;
 
     public float horizontalSpeed = 6;
-    private float jumpForce = 5.0f;
+    private float jumpForce = 18.0f;
+    private float forwardForce = 18.0f;
     private Rigidbody rb;
+    public Rigidbody rbEnemy;
     public SphereCollider col;
     public LayerMask groundLayers;
     public Animation lionAnimation;
-    private Vector3 startingPoint;
+    public Vector3 startingPoint;
+    public Vector3 currentPosition;
+    public AudioSource wolf1;
+    public AudioSource wolf2;
+    public AudioSource wolfAttack;
+    public AudioSource lionDeath;
     void Start()
     {
         rb = GetComponent<Rigidbody>();
         col = GetComponent<SphereCollider>();
+        startingPoint = transform.position;
         lionAnimation.Play("idle");
         startingPoint = transform.position;
+        rb.velocity = new Vector3(0,0,3);
     }
     void Update()
     {
+        if(rb.velocity.z<3)
+        {
+            wolf2.Play();
+            forwardForce = forwardForce - 9.0f;
+            if(rb.velocity.z<1)
+            {
+                rbEnemy.MovePosition(currentPosition);
+                wolfAttack.Play();
+                Debug.Log("You dead");
+                StartCoroutine(delayRespawn());
+            }
+        }
+        float maxSpeed = 3;
+        rb.AddForce(Vector3.forward * jumpForce, ForceMode.Impulse);
+        rb.velocity = Vector3.ClampMagnitude(rb.velocity, maxSpeed);
+        currentPosition = transform.position;
         lionAnimation.Play("run");
-        transform.Translate(Vector3.forward * Time.deltaTime * moveSpeed, Space.World);
+        //transform.Translate(Vector3.forward * Time.deltaTime * moveSpeed, Space.World);
         if (Input.GetKey(KeyCode.LeftArrow) || Input.GetKey(KeyCode.A))
         {
             if(this.gameObject.transform.position.x > LevelBoundary.leftSide)
@@ -41,15 +67,23 @@ public class PlayerMove : MonoBehaviour
         }
             if(Input.GetKey(KeyCode.UpArrow) || Input.GetKey(KeyCode.W))
             {
-                rb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
-                lionAnimation.Play("jump");
+                if(isGrounded())
+                {
+                    rb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+                    lionAnimation.Play("jump");
+                   
+                }
+                else
+                {
+                     //Debug.Log("Player State: " + isGrounded());
+                }
             }
     }
     void OnTriggerEnter(Collider other)
 {
     print("Collision detected with trigger object " + other.name);
+    wolf1.Play();
     lionAnimation.Stop();
-    StartCoroutine(delayRespawn());
 }
 
     private bool isGrounded()
@@ -60,7 +94,16 @@ public class PlayerMove : MonoBehaviour
         IEnumerator delayRespawn()
     {
         Debug.Log("Started Coroutine at timestamp : " + Time.time);
-        yield return new WaitForSeconds(2);
+        lionDeath.Play();
+        yield return new WaitForSeconds(4);
         Debug.Log("Finished Coroutine at timestamp : " + Time.time);
+        StartCoroutine(Reload());
     }
+    private IEnumerator Reload()
+        {
+        yield return new WaitForSeconds(6);
+        SceneManager.UnloadSceneAsync(SceneManager.GetActiveScene().buildIndex);
+        Resources.UnloadUnusedAssets();
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex, LoadSceneMode.Single);
+        }
 }
